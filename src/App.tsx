4f674f47147ef609f4e6ab9b259b65ce95513ca1
etienne-85 <mutonic@gmail.com>
2025-07-18 08:35:50 +0200

    add TW

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,17 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-sm">
+        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+          Tailwind Test Card
+        </div>
+        <p className="text-gray-700 mb-6">
+         This is for testing current tailwind setup is working
         </p>
+        <button className="w-full py-2 px-4 bg-blue-600 text-white rounded hover:bg-blue-700 transition">
+           Hello world
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
